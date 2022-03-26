f7edd120cf21b86fb0b14ea1a35000b7a4b2fdfa
PEDRO BIHAN <pbihan@uc.cl>
2022-03-26 20:47:02 -0300

    top users funcionando

--- users.py
+++ users.py
@@ -4,9 +4,9 @@ usuarios_emisiones = {}
 
 for i in range(len(data_list)):
     if data_list[i]['user']['username'] not in usuarios_emisiones:
-        usuarios_emisiones[data_list[i]['user']['username']] = data_list[i]['user']['statusesCount']
-    elif (data_list[i]['user']['username'] in usuarios_emisiones) & (data_list[i]['user']['statusesCount'] != 'null'):
-        usuarios_emisiones[data_list[i]['user']['username']] += data_list[i]['user']['statusesCount']
+        usuarios_emisiones[data_list[i]['user']['username']] = 1
+    elif (data_list[i]['user']['username'] in usuarios_emisiones):
+        usuarios_emisiones[data_list[i]['user']['username']] += 1
 
 
 sort_retweets = sorted(usuarios_emisiones.items(), key=lambda x: x[1], reverse=True)
